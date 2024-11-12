165c360102a987372c62a3467d08ab895b1598db
BriannaP33 <20102669@mail.wit.ie>
2024-11-12 17:49:54 +0000

    Add task functionality

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import logo from './logo.svg';
 import './App.css';
 import Task from './components/Task';
@@ -12,6 +13,11 @@ function App() {
       {  id: 3, title: "Tidy up", description: "Tidy kitchen", deadline: "Today", done: false }
     ]
   });
+  const [ formState, setFormState ] = useState({
+    title: "",
+    description: "",
+    deadline: ""
+  });
   const doneHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
     tasks[taskIndex].done = !tasks[taskIndex].done;
@@ -23,6 +29,36 @@ function App() {
     tasks.splice(taskIndex, 1);
     setTaskState({tasks});
   } 
+  const formChangeHandler = (event) => {
+    let form = {...formState};
+
+    switch(event.target.name) {
+      case "title":
+          form.title = event.target.value;
+          break;
+      case "description":
+          form.description = event.target.value;
+          break;
+      case "deadline":
+          form.deadline = event.target.value;
+          break;
+      default:
+          form = formState;
+    }
+    setFormState(form);
+    console.log(formState);
+  }
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const tasks = [...taskState.tasks];
+    const form = {...formState};
+
+    form.id = uuidv4();
+    
+    tasks.push(form);
+    setTaskState({tasks});
+  }
   return (
     <div className="container">
       <h1> Tasky </h1>
@@ -35,9 +71,10 @@ function App() {
       done={task.done}
       markDone={() => doneHandler(index)}
       deleteTask = {() => deleteHandler(index)}
+      onChange={(event) => props.change(event)}
     />
   ))} 
-    <AddTaskForm />
+     <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
      </div>
   );
 }
