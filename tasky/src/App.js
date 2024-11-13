0dfac7283a14bfc5abfe898180df2600b66ee593
BriannaP33 <20102669@mail.wit.ie>
2024-11-13 16:50:19 +0000

    Tasky Lab 2 - Exercises done

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -8,15 +8,16 @@ import AddTaskForm from './components/Form';
 function App() {
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      {  id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
-      {  id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
-      {  id: 3, title: "Tidy up", description: "Tidy kitchen", deadline: "Today", done: false }
+      {  id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false, priority: "high" },
+      {  id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false, priority:"low" },
+      {  id: 3, title: "Tidy up", description: "Tidy kitchen", deadline: "Today", done: false, priority: "medium" }
     ]
   });
   const [ formState, setFormState ] = useState({
     title: "",
     description: "",
-    deadline: ""
+    deadline: "",
+    priority: "low"
   });
   const doneHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
@@ -41,6 +42,9 @@ function App() {
           break;
       case "deadline":
           form.deadline = event.target.value;
+          break;
+      case "priority":
+          form.priority = event.target.value;
           break;
       default:
           form = formState;
@@ -67,6 +71,7 @@ function App() {
       title={task.title}
       description={task.description}
       deadline={task.deadline}
+      priority={task.priority}
       key={task.id}
       done={task.done}
       markDone={() => doneHandler(index)}
--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -1,14 +1,31 @@
 import React from 'react';
 
 const Task = (props) => {
+    let priorityColor;
+
+    switch(props.priority){
+        case "low":
+            priorityColor = "#00796b";  
+            break;
+          case "medium":
+            priorityColor = "#ffa000"; 
+            break;
+          case "high":
+            priorityColor = "#d32f2f"; 
+            break;
+          default:
+            priorityColor = "#5bb4c4"; 
+      }
+
     return (
         <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p>{props.description}</p>
+            <p className="priority" style={{color: priorityColor}}>Priority: {props.priority}</p>
             <button onClick={props.markDone} className='doneButton'>Done</button>
             <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
         </div>
-    )
-}
+    );
+};
 export default Task;
